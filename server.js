92a9a9f4b3aab543aace48024226aee440c9ff98
maimbeeb <mariamhabib1@hotmail.com>
2019-09-15 22:28:15 +0530

    Heroku deployment

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+let app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.listen(port, () => {
+  console.log('Server is up on port ${port}');
+});
